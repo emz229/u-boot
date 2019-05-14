9546bca3e72297b841494b6da6e9abc72501c39e
Simon Glass <sjg@chromium.org>
2019-05-14 15:53:37 -0600

    binman: Convert print statements to Python 3
    Update all print statements to be functions, as required by Python 3.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/binman/binman.py
+++ tools/binman/binman.py
@@ -9,6 +9,8 @@
 
 """See README for more information"""
 
+from __future__ import print_function
+
 import glob
 import os
 import sys
@@ -67,13 +69,13 @@ def RunTests(debug, args):
             suite = unittest.TestLoader().loadTestsFromTestCase(module)
         suite.run(result)
 
-    print result
+    print(result)
     for test, err in result.errors:
-        print test.id(), err
+        print(test.id(), err)
     for test, err in result.failures:
-        print err, result.failures
+        print(err, result.failures)
     if result.errors or result.failures:
-      print 'binman tests FAILED'
+      print('binman tests FAILED')
       return 1
     return 0
 
@@ -125,9 +127,9 @@ def RunBinman(options, args):
         try:
             ret_code = control.Binman(options, args)
         except Exception as e:
-            print 'binman: %s' % e
+            print('binman: %s' % e)
             if options.debug:
-                print
+                print()
                 traceback.print_exc()
             ret_code = 1
     return ret_code
